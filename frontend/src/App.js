f121c4673d7bc7f9b7b9b952325d08668990bfee
Fabian Andres Llanos Osorio <fa.llanos@udla.edu.co>
2020-10-14 18:52:28 -0500

    Google Map

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,21 @@
-import React, { Fragment } from 'react';
+import React from "react";
 import Header from "./componentes/Header";
 import Footer from "./componentes/Footer";
-import Producto from "./componentes/Producto";
-import Baner from "./componentes/Baner";
-
+import Mapa from "./componentes/map";
 function App() {
-    return (
-        <Fragment>
-             
-            
-            <Header
-                titulo="Gasolinera Mexico"
-            />
-
-            <Baner
-                Regular="Regular"
-                Premium="Premium"
-                Diesel="Diesel"
-            />
-
-            <Producto/>
-
-            <Footer />
-
-
-        </Fragment>
-    );
-
-
-
+  return (
+    <>
+      <Header titulo="Gasolinera Mexico" />
+      <Baner Regular="Regular" Premium="Premium" Diesel="Diesel" />
+      {/* <Mapa
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?key=AIzaSyCE3vxzOVAM8YnMdNmLoSLJG5TN6cqPqPg`}
+        containerElement={<div style={{ height: "70vh" }}></div>}
+        mapElement={<div style={{ height: "100%" }}></div>}
+        loadingElement={<p>Cargando</p>}
+      /> */}
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ frontend/src/FindStation.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Mapa from "./componentes/map";
+function FindStation() {
+  return <></>;
+}
+export default FindStation;
