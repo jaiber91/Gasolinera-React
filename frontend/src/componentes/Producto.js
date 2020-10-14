471ed03cdecc3092c9f4fe0bce17564798ce4aec
JhonnierGS <gs.170590@gmail.com>
2020-10-14 18:05:15 -0500

    llamando  Api

--- frontend/src/componentes/Producto.js
+++ frontend/src/componentes/Producto.js
@@ -5,8 +5,20 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faAngleRight, faMapMarkerAlt} from '@fortawesome/free-solid-svg-icons';
 
 const Producto = ({product}) => {
-    
 
+    const [equipo, setEquipo] = React.useState([])
+
+    React.useEffect(() =>{
+        obtenerDatos()
+    },[])
+
+    const obtenerDatos = async () =>{
+        const datos = await fetch('https://gasolinerasmexico.herokuapp.com/api/stations')
+        const users = await datos.json()
+        setEquipo(users.data)
+    }
+    
+      
     return (
         <section>
               <section className="locagtion-map">
@@ -25,17 +37,37 @@ const Producto = ({product}) => {
                 <figure className="frame1">
                     <img src={gasFuel} alt=""/>
                     <h2>Regular</h2>
-                    <h2>99999</h2>
+                    <div>
+                      {
+                          equipo.map(item =>(
+                              <h2 key={item.element}>{item.regular}</h2>
+                          ))  
+                      }
+                     </div>  
                 </figure>
                 <figure className="frame2">
                     <img src={gasFuel} alt=""/>
                     <h2>Premium</h2>
-                    <h2>99999</h2>
+                    <div>
+                      {
+                          equipo.map(item =>(
+                              <h2 key={item.element}>{item.premium}</h2>
+                          ))  
+                      }
+                     </div>  
                 </figure>
                 <figure className="frame3">
                     <img src={gasFuel} alt=""/>
                     <h2>Diesel</h2>
-                    <h2>99999</h2>
+                      <div>
+                      {
+                          equipo.map(item =>(
+                              <h2 key={item.element}>{item.diesel}</h2>
+                          ))  
+                      }
+                     </div>  
+                        
+                    
                 </figure> 
             </div>
             <div className= "go-container">
