1aef26fdff4d9c8deb963410e747eccc813f7c8b
JhonnierGS <gs.170590@gmail.com>
2020-10-15 22:38:14 -0500

    agregando Rute

--- frontend/src/componentes/Rute.js
+++ frontend/src/componentes/Rute.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import FindStation from "../FindStation";
-import topStation from "../topStation";
 import App from '../App';
 import Header from './Header';
 function Rute(){
@@ -14,7 +13,6 @@ function Rute(){
         <Switch>
           <Route exact path="/" component={App}/>
           <Route exact path="/FindStation" component={FindStation}/>
-          <Route path="/topStation" component={topStation}/>
       </Switch>
     </BrowserRouter>
       </>
