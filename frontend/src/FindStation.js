de89cecd0ca148c03e1daff82e65f79a87f8bbf7
Fabian Andres Llanos Osorio <fa.llanos@udla.edu.co>
2020-10-15 23:56:19 -0500

    Fix Warning Alerts 2.0

--- frontend/src/FindStation.js
+++ frontend/src/FindStation.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Footer from "./componentes/Footer";
-import Mapa from "./componentes/mapa";
+import Mapa from "./componentes/Mapa";
 function FindStation() {
   return (
     <>
